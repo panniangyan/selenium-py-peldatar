d9d314e7c395cb722a4fd6773e49f0f378b15b37
panniangyan <panniangyan@gmail.com>
2021-06-21 03:07:02 +0200

    016-1 feladat megoldása

---
+++ python_homework/substract.py
@@ -0,0 +1,8 @@
+#Kérjünk be két számot, és írjuk ki a különgségüket. Használd az input() és print() beépített függvényeket.
+
+x = int(input("Kérlek adj meg egy számot: "))
+y = int(input("Kérlek adj meg egy számot: "))
+
+print("A két megadott szám különbsége =", x-y)
+
+
