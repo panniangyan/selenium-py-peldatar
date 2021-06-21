b0a564c64b838360439d3c17a4a57e4d47147bb2
panniangyan <panniangyan@gmail.com>
2021-06-21 17:47:06 +0200

    003 feladat megoldása

---
+++ selenium_homework/bmodul.py
@@ -0,0 +1,4 @@
+#Készíts egy bmodul nevezetű python modult. Hozz benne létre egy változót és tárol benne egy értéket. Egy másik modulban, mondjuk main modul, importáld be a bmodul-ból a változót és használd fel egy kiiratásban.
+
+def var(a):
+    return a
