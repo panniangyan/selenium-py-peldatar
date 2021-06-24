f11f864345376fe7d29d156876a7abbb37d34f47
panniangyan <panniangyan@gmail.com>
2021-06-24 22:53:59 +0200

    new

---
+++ python-homework/szokoev.py
@@ -0,0 +1,47 @@
+#Szökőév?
+# Készíts függvényt, amelyik adott évszámról eldönti, hogy az szökőév-e.
+# Szökőév minden negyedik, nem szökőév minden századik, mégis az minden 400-adik.
+# (2000-ben ezért volt szökőév.) A függvény visszatérési értéke legyen logikai típusú!
+# Tipp( % maradekos osztas operátor)
+# Írj programot, amelyik a felhasználótól évszámokat kér, és mindegyikre kiírja, hogy szökőév-e!
+# Használd az előbb megírt függvényt!
+# Például: ? 2005 Nem szökőév. ? 2000 Szökőév. ? 1980 Szökőév. ? 1900 Nem szökőév.
+# A megoldást egy szokoev.py nevű file-ban kell beadnod.
+
+#evszam = int(input("Adj meg egy évszámot: "))
+
+#def szokoev(evszam):
+#    if (evszam % 4) == 0:
+#        if (evszam % 100) == 0:
+#            if (evszam % 400) == 0:
+#                print(evszam, "Szökőév.")
+#            else:
+#                print(evszam, "Nem szökőév.")
+#        else:
+#            print(evszam, "Szökőév.")
+#    else:
+#        print(evszam, "Nem szökőév.")
+
+
+#https://www.geeksforgeeks.org/program-check-given-year-leap-year/
+
+def checkYear(year):
+    import calendar
+    return (calendar.isleap(year))
+
+
+year = int(input("please give a year: "))
+
+if (checkYear(year)):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
+
+
+
+
+
+
+
+
+
